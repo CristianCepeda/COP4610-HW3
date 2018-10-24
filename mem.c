f745bf86ed1f714c32d4231991e157a948ec5d8a
Marcial <marcialandres@me.com>
2018-10-24 17:13:43 -0400

    Created new file

--- mem.c
+++ mem.c
@@ -9,24 +9,25 @@ int Mem_Init(int size, int policy){
   // void *mmap(void *addr, size_t length, int prot, int flags,
   //                 int fd, off_t offset);
 
-  int returnValue = 0;
-
   void *addr;
   size_t length = size;
-  // int prot = PROT_EXEC;
-  // ---------------------------------------------------------------------------------------------------
-  // int flags = MAP_SHARED;
-  // ---------------------------------------------------------------------------------------------------
-  int fd;
-  // ---------------------------------------------------------------------------------------------------
-  off_t offset = 32;
-  off_t pa_offset = offset & ~(sysconf(_SC_PAGE_SIZE)-1); // WE ARE HARDCODING OUR OFFSET TO BE 32
-  // ---------------------------------------------------------------------------------------------------
 
-  void* p = mmap(addr,length,PROT_EXEC, MAP_SHARED, fd, pa_offset);
+  int returnValue = 0;
 
+  // open the /dev/zero device
+  int fd = open("/dev/zero", O_RDWR);
 
-  return returnValue;
+  // size (in bytes) must be divisible by page size
+  addr = mmap(NULL, length, PROT_WRITE, MAP_PRIVATE, fd, 0);
+
+  if (addr == MAP_FAILED) {
+    perror("mmap");
+    return NULL;
+  }
+
+  // close the device (don't worry, mapping should be unaffected)
+  close(fd);
+  return addr;
 }
 
 void* Mem_Alloc(int size){

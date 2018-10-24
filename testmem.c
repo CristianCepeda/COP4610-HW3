07b16c9a88ccb1f3e15bab906f1ba76c120e0d74
CristianCepeda <ccepe014@fiu.edu>
2018-10-24 08:25:24 -0400

    Worked on OCT 23 together

--- testmem.c
+++ testmem.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+#include <unistd.h> // this is to use sysconf(_SC_PAGESIZE) and this equals 
 #include "mem.h"
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+// #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define REGION_SIZE (10*1024)
 
@@ -13,8 +21,27 @@ void* myalloc(int size)
 }
 
 int Mem_Init(int size, int policy){
+  // void *mmap(void *addr, size_t length, int prot, int flags,
+  //                 int fd, off_t offset);
 
-  return 0;
+  int returnValue = 0;
+
+  void *addr;
+  size_t length = size;
+  // int prot = PROT_EXEC; 
+  // ---------------------------------------------------------------------------------------------------
+  // int flags = MAP_SHARED;
+  // ---------------------------------------------------------------------------------------------------
+  int fd;
+  // ---------------------------------------------------------------------------------------------------
+  off_t offset = 32;
+  off_t pa_offset = offset & ~(sysconf(_SC_PAGE_SIZE)-1); // WE ARE HARDCODING OUR OFFSET TO BE 32
+  // ---------------------------------------------------------------------------------------------------
+
+  void* p = mmap(addr,length,PROT_EXEC, MAP_SHARED, fd, pa_offset);
+  
+
+  return returnValue;
 }
 
 void* Mem_Alloc(int size){
